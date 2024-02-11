b219cafca4812c1d8a1ed1e7b1a9c406f240fda4
tmmarquess <thiago.uchoa18@gmail.com>
2024-02-11 18:02:29 -0400

    removing tls

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { readFileSync } from 'fs';
+// import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: readFileSync('./sslFiles/key.pem'),
-    cert: readFileSync('./sslFiles/cert.pem'),
-  };
+  // const httpsOptions = {
+  //   key: readFileSync('./sslFiles/key.pem'),
+  //   cert: readFileSync('./sslFiles/cert.pem'),
+  // };
 
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule /*, { httpsOptions }*/);
   app.enableCors();
   await app.listen(3001);
 }
